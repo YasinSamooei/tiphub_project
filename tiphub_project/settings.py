a15b95b33c0b21fac5e76cf4eaa59a3bd025886a
Yasin <samooei.y03@gmail.com>
2022-09-13 23:17:37 +0430

    change db from sqlite3 to mysql

--- tiphub_project/settings.py
+++ tiphub_project/settings.py
@@ -102,11 +102,20 @@ WSGI_APPLICATION = 'tiphub_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tiphub_db',
+        'USER':'root',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
-
+#settings of db.sqlite3
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
